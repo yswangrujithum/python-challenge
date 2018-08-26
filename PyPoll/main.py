9ab09876d408b5203937e26fe7d33b173e00ddf5
Yanin Swangrujithum <yaninswangrujithum@Yanins-MacBook-Pro.local>
2018-08-26 01:38:46 -0700

    Fix PyPoll code

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,50 +1,58 @@
 import os
-import csv
-import pandas as pd 
+import csv  
+import pandas as pb
 import numpy as np 
-
-
 
 csvPath = os.path.join("..","Resources","election_data.csv")
 data_output = os.path.join("output_poll_data.txt")
 
-
 print("Election Results")
 print("-----------------------------------------")
 
-def voter_count():
+
+def total_voter():
     with open(csvPath,newline="") as csvfile:
         csvreader = csv.reader(csvfile, delimiter=",")
         csv_header = next(csvreader)
-        voter = sum(1 for row in csvfile)
+        total_vote = sum(1 for row in csvfile)
 
+
+    candidate = []
+    vote_count = []
+    data = {}
+    votes = 0
+    percent_vote = {}
     with open(csvPath,newline="") as csvfile:
-        csvreader = pd.read_csv(csvfile)
-        df = pd.DataFrame(csvreader)
-        candidate_count = df["Candidate"].value_counts()
-        candidate_1 = (candidate_count['Khan'])
-        candidate_2 = (candidate_count['Correy'])
-        candidate_3 = (candidate_count['Li'])
-        candidate_4 = (candidate_count["O'Tooley"])
-        
-        winner = candidate_count.idxmax(axis=1)
+        csvreader = csv.reader(csvfile,delimiter=",")
+        csv_header = next(csvreader)
+        for row in csvreader:
+            votes += 1
+            if row[2] in data.keys():
+                data[row[2]] = data[row[2]] + 1
+            else:
+                data[row[2]] = 1
+    for key,value in data.items():
+        candidate.append(key)
+        vote_count.append(value)
 
-        percent_1 = (candidate_1/voter) * 100
-        round_percent_1 = format(percent_1,'.3f')
-        percent_2 = (candidate_2/voter) * 100
-        round_percent_2 = format(percent_2,'.3f')
-        percent_3 = (candidate_3/voter) * 100
-        round_percent_3 = format(percent_3,'.3f')
-        percent_4 = (candidate_4/voter) * 100
-        round_percent_4 = format(percent_4,'.3f')
-        
-        
-    print(f'Total Votes: {voter}')
+    for key,value in data.items():
+        percent_vote[key] = round((value/votes) * 100,3)
+    
+    winner=""
+    winner_count = 0
+    for key in data.keys():
+        if data[key] > winner_count:
+            winner = key
+            winner_count = data[key]
+
+
+
+    print(f'Total Votes: {total_vote}')
     print('-------------------------------------------')
-    print(f'Khan: {round_percent_1}% ({candidate_1})')
-    print(f'Correy: {round_percent_2}% ({candidate_2})')
-    print(f'Li: {round_percent_3}% ({candidate_3})')
-    print(f"O'Tooley: {round_percent_4}% ({candidate_4})")
+    print(f'Khan: {list(percent_vote.values())[0]}%  ({vote_count[0]})')
+    print(f'Correy: {list(percent_vote.values())[1]}%  ({vote_count[1]})')
+    print(f'Li: {list(percent_vote.values())[2]}%  ({vote_count[2]})')
+    print(f"O'Tooley: {list(percent_vote.values())[3]}%  ({vote_count[3]})")
     print('-------------------------------------------')
     print(f'Winner: {winner}')
     print('-------------------------------------------')
@@ -52,12 +60,17 @@ def voter_count():
     data_output = open('output_poll_data.txt','w')
     data_output.write('Election Results\n')
     data_output.write('----------------------------------\n')
-    data_output.writelines(f'Khan: {round_percent_1}% ({candidate_1})\n')
-    data_output.writelines(f'Correy: {round_percent_2}% ({candidate_2})\n')
-    data_output.writelines(f'Li: {round_percent_3}% ({candidate_3})\n')
-    data_output.writelines(f"O'Tooley: {round_percent_4}% ({candidate_4})\n")
+    data_output.writelines(f'Khan: {list(percent_vote.values())[0]}%  ({vote_count[0]})\n')
+    data_output.writelines(f'Correy: {list(percent_vote.values())[1]}% ({vote_count[1]})\n')
+    data_output.writelines(f'Li: {list(percent_vote.values())[2]}% ({vote_count[2]})\n')
+    data_output.writelines(f"O'Tooley: {list(percent_vote.values())[3]}% ({vote_count[3]})\n")
     data_output.write('----------------------------------\n')
     data_output.write(f'Winner: {winner}\n')
     data_output.write('----------------------------------\n')
-voter_count()
+    
+
+
+total_voter()
+
+
 
